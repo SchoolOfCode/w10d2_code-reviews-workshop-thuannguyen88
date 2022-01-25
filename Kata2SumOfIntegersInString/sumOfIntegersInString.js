872b3ca88f0348474ac66a0f438402ba4a7a5e2a
thuannguyen88 <getaholdofthuan@gmail.com>
2022-01-25 11:51:14 +0000

    code review to help solve kata

--- Kata2SumOfIntegersInString/sumOfIntegersInString.js
+++ Kata2SumOfIntegersInString/sumOfIntegersInString.js
@@ -1,5 +1,18 @@
 function sumOfIntegersInString(str) {
   // Code here
+  let strArray = str.split(",");
+
+  let sum = strArray.reduce(function (total, num) {
+    if (typeof strArray[i] === "number") {
+      return total + num;
+    } else return total + 0;
+  });
+  return sum;
 }
+
+// split string
+// sum all the integers
+// ignore non integers
+// use typeof?
 
 export default sumOfIntegersInString;
